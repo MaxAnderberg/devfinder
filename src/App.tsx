68992821c4756603583fef082f8fbe96f6050136
Max Anderberg <maxanderberg@gmail.com>
2023-02-16 17:47:25 +0000

    worked out the main layout of desktop vieew

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import SearchBar from './components/SearchBar/SearchBar'
 import SearchResult from './components/SearchResult/SearchResult'
 import './App.css'
+import LightIcon from './assets/icon-sun.svg'
 
 function App() {
   const [searchDev, setSearchDev] = useState('')
+  // const [isDarkTheme, setDarkTheme] = useState(false)
 
   return (
-    <div className="bg-slate-200 flex flex-col justify-center items-center h-screen w-full">
+    <div className="bg-slate-200 flex flex-col justify-center items-center h-screen w-3/4">
       <div className='w-full flex justify-between'>
-        <p>Devfinder</p> 
-        <div>
-          <span>Light</span> 
-          <span>Icon</span>
+        <p className='lowercase text-white font-bold text-2xl'>Devfinder</p> 
+        <div className='flex'>
+          <img src={LightIcon} alt="" />
+          <span className='uppercase text-white font-bold'>Light</span> 
         </div>
       </div>
       <SearchBar setSearchDev={setSearchDev}/>
