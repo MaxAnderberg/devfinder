be90a6b8fbae4a96fe9003dbd9789dc89bb014ab
Max Anderberg <maxanderberg@gmail.com>
2023-02-17 16:37:08 +0000

    aligned children in searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,8 +4,8 @@ const SearchBar = (props:any) => {
 return (
         <div className="flex justify-between bg-[#1E2A47] w-full rounded-lg">
             <form className="flex m-2">
-                <img src={SearchIcon} className='m-2' alt="a magnifyier" />
-                <input className="rounded-lg p-4 border-0 bg-transparent" type="text" placeholder="Search for a developer" />
+                <img src={SearchIcon} className='m-auto' alt="a magnifyier" />
+                <input className="rounded-lg ml-2 p-4 border-0 bg-transparent" type="text" placeholder="Search for a developer" />
             </form>
             <button className="bg-blue-600 p-4 rounded-lg text-white m-2">Search</button>
         </div>
