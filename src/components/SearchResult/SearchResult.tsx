ac82586701dabf61e5a005af0d2f43e3a388e513
Max Anderberg <maxanderberg@gmail.com>
2023-02-17 10:50:06 +0000

    fixed grid layout

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,3 +1,4 @@
+import DummyIcon from '../../assets/react.svg'
 const SearchResult = () => {
   const developer = {
     userName: "maxanderberg",
@@ -10,22 +11,26 @@ const SearchResult = () => {
     bio: "There was once a padawan..."
   }
   return (
-    <div className="bg-[#1E2A47] flex flex-col mt-6 w-full items-center">
-      <p>{developer.userName}</p>
-      {developer.bio}
-      <div className="bg-[#141D2F] grid grid-rows-2 grid-cols-2 justify-center rounded-lg w-1/2">
-          <p>Repos</p>
-          <p>Followers</p>
-          <p>Followers</p>
-          <p>{developer.repositories}</p>
-          <p>{developer.followers}</p>
-          <p>{developer.following}</p>
-     </div>
-      <div>
-        {developer.location}
-        {developer.twitter}
-        {developer.email}
-      </div>
+    <div className="grid grid-cols-4 w-full">
+      <img src={DummyIcon} alt="" className='w-1/2 m-auto'/>
+      <div className="bg-[#1E2A47] flex flex-col mt-6 w-full items-center col-span-3 text-white">
+        <p>{developer.userName}</p>
+        {developer.bio}
+        <div className="bg-[#141D2F] grid grid-rows-2 grid-cols-2 justify-center rounded-lg w-1/2">
+            <p>Repos</p>
+            <p>Followers</p>
+            <p>Followers</p>
+            <p>{developer.repositories}</p>
+            <p>{developer.followers}</p>
+            <p>{developer.following}</p>
+        </div>
+        <div>
+          {developer.location}
+          {developer.twitter}
+          {developer.email}
+        </div>
+    </div>
+   
     </div>
   )
 }
