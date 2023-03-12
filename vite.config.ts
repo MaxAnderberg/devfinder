8f0eb1924b7edcf114fa3641621803fe8812219a
Max <maxanderberg@gmail.com>
2023-03-12 16:08:41 +0100

    added vite svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  plugins: [
+    svgr({
+      svgrOptions: {
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              removeUselessStrokeAndFill: true,
+              removeAttrs: {
+                attrs: "path(stroke|fill)",
+              },
+            },
+          ],
+        },
+      },
+    }),
+  ],
+});
