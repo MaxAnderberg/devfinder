3175f2b4fa66e11362311346dc3cbeaf27088260
Max <maxanderberg@gmail.com>
2023-03-12 16:08:22 +0100

    made the entire app wider

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
   }, [searchDeveloperHandler]);
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-4/6">
+    <div className="flex flex-col justify-center items-center h-screen w-5/6">
       <div className="w-full flex justify-between">
         <p className="lowercase text-white font-bold text-2xl">Devfinder</p>
         <div className="flex mr-4 mb-12">
