3215bfdbb117f7d6a727a80408598fe10f998267
Max <maxanderberg@gmail.com>
2023-03-20 08:36:52 +0100

    added handler for input field

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 import SearchIcon from "../../assets/icon-search.svg";
 
 const SearchBar = (props: any) => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+    const cleanedSearchText = searchText.trim();
+    props.setSearchDev(cleanedSearchText);
+    setSearchText("");
+  };
+
+  const handleInputChange = (e: any) => {
+    setSearchText(e.target.value);
+  };
+
   return (
     <div className="flex justify-between bg-[#1E2A47] w-full rounded-2xl">
-      <form className="flex m-2 w-1/2 ml-6">
+      <form onSubmit={handleSearch} className="flex m-2 w-1/2 ml-6">
         <img src={SearchIcon} className="m-auto" alt="a magnifyier" />
         <input
           className="rounded-lg ml-2 p-4 border-0  w-full placeholder-white bg-[#1E2A47] text-white focus:ring-0 focus:outline-none"
           type="text"
           placeholder="Search GitHub username..."
+          onChange={handleInputChange}
+          value={searchText}
         />
       </form>
       {props.foundSearch ? (
