4f897086e7d1f21a6e153d2f68b5bef0bcc451c5
Max <maxanderberg@gmail.com>
2023-04-18 08:30:43 +0200

    finished styling of search bar mobile

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -16,16 +16,16 @@ const SearchBar = (props: any) => {
   };
 
   return (
-    <div className="bg-[#1E2A47] w-[730px] rounded-2xl">
+    <div className="bg-[#1E2A47] md:w-[730px] w-[337px] rounded-2xl">
       <form onSubmit={handleSearch} className="flex">
-        <div className="m-auto ml-8">
+        <div className="m-auto ml-4 md:ml-8">
           <SearchIcon
             src={SearchIcon}
             className=" text-[#0079FF] w-[24px] h-[24px]"
           />
         </div>
         <input
-          className="rounded-lg p-4 border-0 w-full placeholder-white bg-[#1E2A47] text-white focus:ring-0 focus:outline-none"
+          className="rounded-lg py-4 md:p-4 border-0 w-full placeholder-white bg-[#1E2A47] text-white focus:ring-0 focus:outline-none text-xs ml-2"
           type="text"
           placeholder="Search GitHub username..."
           onChange={handleInputChange}
@@ -40,7 +40,7 @@ const SearchBar = (props: any) => {
         )}
         <button
           type="submit"
-          className="bg-button-blue px-6 py-3 rounded-xl text-white m-2 font-bold hover:bg-button-hover"
+          className="bg-button-blue h-[64px] w-[84px] rounded-xl text-white m-2 font-bold hover:bg-button-hover flex-none"
         >
           Search
         </button>
