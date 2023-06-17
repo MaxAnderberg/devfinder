0ad43069fd0f04b62ee05c8bbd2f519cf7526263
Max <maxanderberg@gmail.com>
2023-06-17 19:20:43 +0200

    added dark and light theme colors

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -8,31 +8,31 @@ const SearchResult = (props: any) => {
   const notAvailable = "Not Available";
 
   return (
-    <div className="lg:flex bg-[#1E2A47] mt-4 md:mt-8 rounded-xl md:p-12 p-6 w-[337px] md:w-[573px] lg:w-[730px]">
+    <div className="lg:flex bg-white dark:bg-dark-secondary mt-4 md:mt-8 rounded-xl md:p-12 p-6 w-[337px] md:w-[573px] lg:w-[730px]">
       <div
-        className="grid grid-rows-3 grid-cols-1 w-full text-white "
+        className="grid grid-rows-3 grid-cols-1 w-full text-white"
         style={{ gridTemplateRows: "auto 1fr auto" }}
       >
         {/* First Section */}
-        <div className="grid  grid-rows-[auto,1fr] md:grid-cols-[1fr-auto] lg:grid-cols-[0.5fr,2fr,auto] lg:mb-0  ">
+        <div className="grid grid-rows-[auto,1fr] md:grid-cols-[1fr-auto] lg:grid-cols-[0.5fr,2fr,auto] lg:mb-0">
           <UserHeader developerResult={props.developerResult} />
         </div>
         {/* Second Section */}
         <div>
-          <div className="bg-[#141D2F] grid grid-rows-1 grid-cols-3 justify-items-center rounded-lg py-[15px] px-[15px] w-full justify-self-center align-self-center mb-[24px] mb:w-[493px] lg:row-start-1 lg:row-end-1 lg:w-[480px] lg:ml-[162px] lg:mt-[-15px]">
+          <div className="bg-[#F6F8FF] dark:bg-[#141D2F] grid grid-rows-1 grid-cols-3 justify-items-center rounded-lg py-[15px] px-[15px] w-full justify-self-center align-self-center mb-[24px] mb:w-[493px] lg:row-start-1 lg:row-end-1 lg:w-[480px] lg:ml-[162px] lg:mt-[-15px]">
             <div>
-              <p className="text-xs mb-[8px]">Repos</p>
-              <p className="font-bold text-center">
+              <p className="text-xs mb-[8px] text-[#4B6A9B]">Repos</p>
+              <p className="font-bold text-center text-[#2B3442]">
                 {props.developerResult.public_repos}
               </p>
             </div>
             <div>
-              <p className="text-xs mb-[8px]">Followers</p>
-              <p className="font-bold">{props.developerResult.followers}</p>
+              <p className="text-xs mb-[8px] text-[#4B6A9B]">Followers</p>
+              <p className="font-bold text-[#2B3442]">{props.developerResult.followers}</p>
             </div>
             <div>
-              <p className="text-xs mb-[8px]">Following</p>
-              <p className="font-bold">{props.developerResult.following}</p>
+              <p className="text-xs mb-[8px] text-[#4B6A9B]">Following</p>
+              <p className="font-bold text-[#2B3442]">{props.developerResult.following}</p>
             </div>
           </div>
         </div>
@@ -43,56 +43,56 @@ const SearchResult = (props: any) => {
               <Location
                 className={`w-[20px] text-red-500 ${
                   props.developerResult.location === null
-                    ? "text-[#697C9A]"
-                    : "text-white"
+                    ? "text-[#697C9A] text-opacity-50"
+                    : "text-[#4B6A9B]"
                 }`}
               />
               {props.developerResult.location !== null ? (
-                <p>{props.developerResult.location}</p>
+                <p className="text-[#4B6A9B]">{props.developerResult.location}</p>
               ) : (
-                <p className="text-white">{notAvailable}</p>
+                <p className="text-white text-opacity-50">{notAvailable}</p>
               )}
             </div>
             <div className="flex gap-x-[16px] lg:ml-[-10px]">
               <Twitter
                 className={`w-[20px] text-white-500 ${
                   props.developerResult.twitter_username === null
-                    ? "text-[#697C9A]"
-                    : "text-white"
+                    ? "text-[#4B6A9B] text-opacity-50"
+                    : "text-[#4B6A9B]"
                 }`}
               />
               {props.developerResult.twitter_username !== null ? (
-                <p>{props.developerResult.twitter_username}</p>
+                <p className="text-[#4B6A9B]">{props.developerResult.twitter_username}</p>
               ) : (
-                <p className="text-slate-600">{notAvailable}</p>
+                <p className="text-[#4B6A9B] text-opacity-50">{notAvailable}</p>
               )}
             </div>
             <div className="flex gap-x-[16px] ">
               <Blog
                 className={`w-[20px] text-gray-500 ${
                   props.developerResult.blog === null
-                    ? "text-[#697C9A]"
-                    : "text-white"
+                    ? "text-[#4B6A9B] dark:text-[#697C9A]"
+                    : "text-[#4B6A9B] dark:text-white"
                 }`}
               />
               {props.developerResult.blog !== null ? (
-                <p>{props.developerResult.blog}</p>
+                <p className="text-[#4B6A9B]">{props.developerResult.blog}</p>
               ) : (
-                <p className="text-slate-600">{notAvailable}</p>
+                <p className="text-[#4B6A9B] text-opacity-50 ">{notAvailable}</p>
               )}
             </div>
             <div className="flex gap-x-[16px] lg:ml-[-10px]">
               <Company
-                className={`w-[20px] text-pink-500 ${
+                className={`w-[20px] text-[#4B6A9B] ${
                   props.developerResult.company === null
                     ? "text-[#697C9A]"
-                    : "text-white"
+                    : "text-[#4B6A9B]"
                 }`}
               />
               {props.developerResult.company !== null ? (
-                <p>{props.developerResult.company}</p>
+                <p className="text-[#4B6A9B]">{props.developerResult.company}</p>
               ) : (
-                <p className="text-slate-600">{notAvailable}</p>
+                <p className="text-[#4B6A9B] text-opacity-50">{notAvailable}</p>
               )}
             </div>
           </div>
